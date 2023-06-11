1b841e7989c5b2e5758ac46352634ea1ac9298c8
Phan Minh Nhựt <phanminhnhut.114@gmail.com>
2023-06-11 10:08:12 +0700

    fix node responsive

--- src/pages/Staff/components/LoRaDashboard/components/Node.tsx
+++ src/pages/Staff/components/LoRaDashboard/components/Node.tsx
@@ -9,7 +9,7 @@ const Node = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="rounded-[6px] flex flex-col justify-center items-center min-w-[362px] min-h-[275px] border-2 border-gray-300 text-t4 font-medium text-black">
+    <div className="rounded-[6px] flex flex-col justify-center items-center w-full xl:w-[362px] h-[275px] border-2 border-gray-300 text-t4 font-medium text-black">
       {/* images */}
       <div
         onClick={() => navigate('/staff/lora-data')}
