3c3930b859cd3eb7098abb5f7bf3aa6a43689005
thinhdev3820 <thinhdinh.dev.3820@gmail.com>
2023-06-21 09:44:43 +0700

    all

---
+++ src/pages/Staff/Nodes/components/LoRaData/components/CreateModal.tsx
@@ -0,0 +1,242 @@
+import { Modal } from '@mui/material';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { createSensors, editSensors } from 'src/redux/slices/loraDataSlice';
+import { useAppDispatch } from 'src/redux/store/hooks';
+
+interface FormProps {
+  setOpenModal: any;
+  setUpdateData: any;
+  editData: any;
+  nodeId: any;
+}
+
+const Form: React.FC<FormProps> = (props) => {
+  const { setOpenModal, setUpdateData, editData, nodeId } = props;
+  const dispatch = useAppDispatch();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues: editData });
+
+  // ** handle submit form
+  const onSubmit = (data: any) => {
+    if (editData) {
+      const req = {
+        id: editData.id,
+        type: data.type,
+        power: data.power,
+        size: data.size,
+        productLine: data.productLine,
+        interval: data.interval,
+        location: data.location,
+      };
+      console.log(req);
+      dispatch(editSensors(req)).then(() => {
+        setOpenModal(false);
+        setUpdateData((prev: boolean) => !prev);
+      });
+    } else {
+      dispatch(createSensors({ req: data, nodeId: nodeId })).then(() => {
+        setOpenModal(false);
+        setUpdateData((prev: boolean) => !prev);
+      });
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* sensorId and Type */}
+      <div className="flex sm:gap-5  sm:flex-row flex-col w-full">
+        <div className="w-full">
+          <label className="text-t3 font-semibold text-[#424856]">Sensor Id</label>
+          <input
+            disabled={editData}
+            type="text"
+            className={`block w-full h-[36px] ${errors?.sensorId ? 'mb-[5px]' : 'mb-[10px]'} ${editData ? 'cursor-not-allowed' : ''
+              } p-[12px] text-t3 sm:text-t3 font-poppins bg-[#F3F4F6] rounded-[5px] focus:outline-primary`}
+            {...register('sensorId', {
+              required: true,
+            })}
+          />
+          {errors?.sensorId?.type === 'required' && <p className="mb-[5px] text-danger text-[14px]">Sensor Id is required</p>}
+        </div>
+        <div className="w-full">
+          <label className="text-t3 font-semibold text-[#424856]">Sensor Type</label>
+          <select
+            className={`block w-full h-[36px] ${errors?.type ? 'mb-[5px]' : 'mb-[10px]'
+              } pl-[10px] text-t3 sm:text-t3 font-poppins bg-[#F3F4F6] rounded-[5px] focus:outline-primary`}
+            {...register("type", {
+              required: true,
+            })}>
+            <option value="TEMPERATURE">Temperature</option>
+            <option value="LIGHT">Light</option>
+            <option value="HUMIDITY">Humidity</option>
+            <option value="SMOKE">Smoke</option>
+          </select>
+          {errors?.type?.type === 'required' && (
+            <p className="mb-[5px] text-danger text-[14px]">Type is required</p>
+          )}
+        </div>
+      </div>
+      {/* Min Max Threshold */}
+      <div className="flex sm:gap-5  sm:flex-row flex-col w-full">
+        <div className="w-full">
+          <label className="text-t3 font-semibold text-[#424856]">Min Threshold</label>
+          <input
+            type="number"
+            disabled={editData}
+            className={`block w-full h-[36px] ${errors?.minThreshold ? 'mb-[5px]' : 'mb-[10px]'} ${editData ? 'cursor-not-allowed' : ''
+              } p-[12px] text-t3 sm:text-t3 font-poppins bg-[#F3F4F6] rounded-[5px] focus:outline-primary`}
+            {...register('minThreshold', {
+              required: true,
+            })}
+          />
+          {errors?.minThreshold?.type === 'required' && (
+            <p className="mb-[5px] text-danger text-[14px]">Min Threshold required</p>
+          )}
+        </div>
+        <div className="w-full">
+          <label className="text-t3 font-semibold text-[#424856]">Max Threshold</label>
+          <input
+            type="number"
+            disabled={editData}
+            className={`block w-full h-[36px] ${errors?.maxThreshold ? 'mb-[5px]' : 'mb-[10px]'} ${editData ? 'cursor-not-allowed' : ''
+              } p-[12px] text-t3 sm:text-t3 font-poppins bg-[#F3F4F6] rounded-[5px] focus:outline-primary`}
+            {...register('maxThreshold', {
+              required: true,
+            })}
+          />
+          {errors?.maxThreshold?.type === 'required' && (
+            <p className="mb-[5px] text-danger text-[14px]">Max Threshold required</p>
+          )}
+        </div>
+      </div>
+      {/* interval */}
+      <>
+        <label className="text-t3 font-semibold text-[#424856]">Interval</label>
+        <input
+          type="number"
+          className={`block w-full h-[36px] ${errors?.interval ? 'mb-[5px]' : 'mb-[10px]'
+            } p-[12px] text-t3 sm:text-t3 font-poppins bg-[#F3F4F6] rounded-[5px] focus:outline-primary`}
+          {...register('interval', {
+            required: true,
+          })}
+        />
+        {errors?.interval?.type === 'required' && (
+          <p className="mb-[5px] text-danger text-[14px]">Interval is required</p>
+        )}
+      </>
+      {/* location */}
+      <>
+        <label className="text-t3 font-semibold text-[#424856]">Location</label>
+        <input
+          type="text"
+          className={`block w-full h-[36px] ${errors?.location ? 'mb-[5px]' : 'mb-[10px]'
+            } p-[12px] text-t3 sm:text-t3 font-poppins bg-[#F3F4F6] rounded-[5px] focus:outline-primary`}
+          {...register('location', {
+            required: false,
+          })}
+        />
+        {errors?.location?.type === 'required' && (
+          <p className="mb-[5px] text-danger text-[14px]">Location is required</p>
+        )}
+      </>
+      {/* Power and Size */}
+      <div className="flex sm:gap-5  sm:flex-row flex-col w-full">
+        <div className="w-full">
+          <label className="text-t3 font-semibold text-[#424856]">Power</label>
+          <input
+            type="text"
+            className={`block w-full h-[36px] ${errors?.power ? 'mb-[5px]' : 'mb-[10px]'
+              } p-[12px] text-t3 sm:text-t3 font-poppins bg-[#F3F4F6] rounded-[5px] focus:outline-primary`}
+            {...register('power', {
+              required: false,
+            })}
+          />
+          {errors?.power?.type === 'required' && (
+            <p className="mb-[5px] text-danger text-[14px]">Power is required</p>
+          )}
+        </div>
+        <div className="w-full">
+          <label className="text-t3 font-semibold text-[#424856]">Size</label>
+          <input
+            type="text"
+            className={`block w-full h-[36px] ${errors?.size ? 'mb-[5px]' : 'mb-[10px]'
+              } p-[12px] text-t3 sm:text-t3 font-poppins bg-[#F3F4F6] rounded-[5px] focus:outline-primary`}
+            {...register('size', {
+              required: false,
+            })}
+          />
+          {errors?.size?.type === 'required' && (
+            <p className="mb-[5px] text-danger text-[14px]">Size is required</p>
+          )}
+        </div>
+      </div>
+      {/* productLine */}
+      <>
+        <label className="text-t3 font-semibold text-[#424856]">Product Line</label>
+        <input
+          type="text"
+          className={`block w-full h-[36px] ${errors?.productLine ? 'mb-[5px]' : 'mb-[10px]'
+            } p-[12px] text-t3 sm:text-t3 font-poppins bg-[#F3F4F6] rounded-[5px] focus:outline-primary`}
+          {...register('productLine', {
+            required: false,
+          })}
+        />
+        {errors?.productLine?.type === 'required' && (
+          <p className="mb-[5px] text-danger text-[14px]">Product Line is required</p>
+        )}
+      </>
+
+      {/* buttons */}
+      <div className="flex justify-end items-center gap-4 mt-10">
+        <button
+          onClick={() => setOpenModal(false)}
+          className="border-primary border text-primary text-t3 font-medium px-5 py-1 rounded-[6px]"
+        >
+          Cancel
+        </button>
+        <button type="submit" className=" text-white bg-primary text-t3 font-medium px-5 py-1 rounded-[6px]">
+          Save
+        </button>
+      </div>
+    </form>
+  );
+};
+
+interface CreateModalProps {
+  openModal: boolean;
+  setOpenModal: any;
+  setUpdateData: any;
+  editData: any;
+  nodeId: any;
+}
+
+const CreateModal: React.FC<CreateModalProps> = (props) => {
+  const { openModal, setOpenModal, setUpdateData, editData, nodeId } = props;
+  console.log(nodeId);
+  const [editNodeData, setEditNodeData] = useState<any>();
+
+  return (
+    <Modal open={openModal} onClose={() => setOpenModal(false)}>
+      <div
+        className="fixed left-[50%]
+  top-[50%] translate-y-[-50%] translate-x-[-50%] sm:w-[500px] w-full bg-white rounded-[4px] py-4 font-poppins"
+      >
+        {/* header */}
+        <div className="px-[25px] pb-4 border-b border-b-[#F3F4F6] text-t7 font-medium">
+          {editData ? 'Edit Sensor' : 'Create new Sensor'}
+        </div>
+        {/* body */}
+        <div className="px-[25px] my-3">
+          <Form setOpenModal={setOpenModal} setUpdateData={setUpdateData} editData={editData} nodeId={nodeId} />
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default CreateModal;
--- src/pages/Staff/Nodes/components/LoRaData/components/SensorList.tsx
+++ src/pages/Staff/Nodes/components/LoRaData/components/SensorList.tsx
@@ -1,40 +1,186 @@
+import { IconButton, Tooltip } from '@mui/material';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useState } from 'react';
+import plus from 'src/assets/images/plus_white.svg';
 import sensor from 'src/assets/images/sensor.svg';
 import actions from 'src/assets/images/sensorAction.svg';
-import { IconButton } from '@mui/material';
+import { deleteSensors } from 'src/redux/slices/loraDataSlice';
+import { useAppDispatch } from 'src/redux/store/hooks';
+import ConfirmModal from 'src/share/components/ConfirmModal';
+import { useOutsideClick } from 'src/share/hooks/useOutSideClick';
+import { SensorProps, UsersProps } from 'src/utils/interface';
+import CreateModal from './CreateModal';
 
-const ListItem: React.FC<{ title: string }> = (props) => {
-  const { title } = props;
+interface Props {
+  currentUser: UsersProps;
+  sensorList: SensorProps[];
+  setUpdateData: any;
+  editSensorData: any;
+  setEditSensorData: any;
+  nodeId: any;
+}
+
+interface ListItemProps {
+  sensorData: any,
+  title: string,
+  setOpenEditModal: any,
+  setUpdateData: any;
+  setEditSensorData: any;
+}
+
+const ListItem: React.FC<ListItemProps> = (props) => {
+  const { sensorData, title, setOpenEditModal, setUpdateData, setEditSensorData } = props;
+  const [openOptions, setOpenOptions] = useState(false);
+  const [openConfirm, setOpenConfirm] = useState(false);
+  const dispatch = useAppDispatch();
+  const ref = useOutsideClick(() => {
+    if (openOptions) {
+      setOpenOptions(false);
+    }
+  });
+
+  const handleDeleteSensor = () => {
+    dispatch(deleteSensors(sensorData.id)).then(() => {
+      setUpdateData((prev: any) => !prev);
+      setOpenConfirm(false);
+    });
+  };
+
+  // handle edit node
+  const handleEditNode = () => {
+    setOpenEditModal(true);
+    setEditSensorData(sensorData);
+  };
+
   return (
-    <div className="w-full flex justify-between items-center pb-2 mb-2 border-b border-b-[#D9D9D9]">
-      <div className="flex sm:flex-row flex-col sm:items-center gap-3">
-        <div className="w-[133px] h-[68px] bg-[#F3F4F6] border border-[#BDC1CA] flex justify-center items-center">
-          <img src={sensor} className="object-contain" />
+    <>
+      {openConfirm && (
+        <ConfirmModal
+          description="Delete this node?"
+          openModal={openConfirm}
+          setOpenModal={setOpenConfirm}
+          rowData={sensorData.id}
+          handleConfirm={handleDeleteSensor}
+        />
+      )}
+      <div className="w-full flex justify-between items-center pb-2 mb-2 border-b border-b-[#D9D9D9]">
+        <div className="flex sm:flex-row flex-col sm:items-center gap-3">
+          <div className="w-[133px] h-[68px] bg-[#F3F4F6] border border-[#BDC1CA] flex justify-center items-center">
+            <img src={sensor} className="object-contain" />
+          </div>
+          {/* <p className="text-t4">{title}</p> */}
+          <div className="flex flex-col">
+            <p className="text-t4 font-semibold text-black">{sensorData.sensorId}</p>
+            <p className="text-t3 font-medium text-black">ID: {sensorData.sensorId}</p>
+          </div>
+
         </div>
-        <p className="text-t4">{title}</p>
+        <div className="relative">
+          <Tooltip title={'Options'} placement="top">
+            <IconButton
+              disabled={openOptions}
+              onClick={() => {
+                if (!openOptions) setOpenOptions(true);
+              }}
+            >
+              <img src={actions} className="object-contain" />
+            </IconButton>
+          </Tooltip>
+          {/* options menu */}
+          <AnimatePresence>
+            {openOptions && (
+              <motion.ul
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5, type: 'spring' }}
+                exit={{ opacity: 0 }}
+                ref={ref}
+                className="absolute z-10 top-15 left-[25%] transform translate-x-[-50%] bg-white border shadow-sm rounded-[5px] font-medium text-t3"
+              >
+                <li onClick={() => handleEditNode()} className="cursor-pointer py-2 px-3 hover:bg-gray-100">
+                  Edit
+                </li>
+                <li onClick={() => setOpenConfirm(true)} className="cursor-pointer py-2 px-3 hover:bg-gray-100">
+                  Delete
+                </li>
+              </motion.ul>
+            )}
+          </AnimatePresence>
+        </div>
       </div>
-      <IconButton>
-        <img src={actions} className="object-contain" />
-      </IconButton>
-    </div>
+    </>
   );
 };
 
-const SensorList = () => {
+const SensorList: React.FC<Props> = (props) => {
+  const {
+    sensorList,
+    setUpdateData,
+    currentUser,
+    editSensorData,
+    setEditSensorData,
+    nodeId,
+  } = props;
+  const [openModal, setOpenModal] = useState(false);
+
   return (
-    <div className="p-5 border-[#B4BECF] border bg-white rounded w-full">
-      <p className="text-t7 font-semibold mb-3">Sensors list</p>
-      <div className="w-full justify-between flex">
-        <p className="text-t4">Name</p>
-        <p className="text-t4">Actions</p>
+    <>
+      {openModal && (
+        <CreateModal
+          openModal={openModal}
+          setOpenModal={setOpenModal}
+          setUpdateData={setUpdateData}
+          editData={editSensorData}
+          nodeId={nodeId}
+        />
+      )}
+      <div className="p-5 border-[#B4BECF] border bg-white rounded w-full">
+        <div className="flex justify-between">
+          <p className="text-t7 font-semibold mb-3">Sensors list</p>
+          <button
+            onClick={() => {
+              setOpenModal(true);
+              setEditSensorData(undefined);
+            }}
+            className="mb-[24px]"
+          >
+            <div className="rounded-full bg-primary hover:bg-primary-400 w-[50px] h-[50px] flex justify-center items-center">
+              <img src={plus} className="object-cover w-[25px] h-[25px]" />
+            </div>
+          </button>
+        </div>
+        <div className="w-full justify-between flex">
+          <p className="text-t4">Name</p>
+          <p className="text-t4">Actions</p>
+        </div>
+        {/* list */}
+        <div className="my-5">
+          {sensorList.length > 0 ? (
+            <>
+              {sensorList.map((sensor: SensorProps) => (
+                <div key={sensor.id}>
+                  <ListItem
+                    sensorData={sensor}
+                    title="Water sensors"
+                    setOpenEditModal={setOpenModal}
+                    setUpdateData={setUpdateData}
+                    setEditSensorData={setEditSensorData}
+                  />
+                </div>
+              ))}
+            </>
+          ) : (
+            <>
+              {currentUser.role == 'USER' && (
+                <p className="my-5 font-medium text-danger">
+                  No sensors have been configure yet, Please reach out to our staff for asistance
+                </p>
+              )}
+            </>
+          )}
+        </div>
       </div>
-      {/* list */}
-      <div className="my-5">
-        <ListItem title="Water sensors" />
-        <ListItem title="Temperature sensors" />
-        <ListItem title="Humidity sensors" />
-        <ListItem title="Dust sensors" />
-      </div>
-    </div>
+    </>
   );
 };
 
