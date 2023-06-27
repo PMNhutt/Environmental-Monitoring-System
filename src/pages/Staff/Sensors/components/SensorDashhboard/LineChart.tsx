778aaeed30723d38f38306de549f5146d99a294e
Phan Minh Nhựt <phanminhnhut.114@gmail.com>
2023-06-27 23:30:10 +0700

    Update LineChart.tsx

--- src/pages/Staff/Sensors/components/SensorDashhboard/LineChart.tsx
+++ src/pages/Staff/Sensors/components/SensorDashhboard/LineChart.tsx
@@ -62,64 +62,64 @@ const getOrCreateLegendList = (chart: any, id: any) => {
   return listContainer;
 };
 
-const htmlLegendPlugin = {
-  id: 'htmlLegend',
-  afterUpdate(chart: any, args: any, options: any) {
-    const ul = getOrCreateLegendList(chart, options.containerID);
+// const htmlLegendPlugin = {
+//   id: 'htmlLegend',
+//   afterUpdate(chart: any, args: any, options: any) {
+//     const ul = getOrCreateLegendList(chart, options.containerID);
 
-    // Remove old legend items
-    while (ul.firstChild) {
-      ul.firstChild.remove();
-    }
+//     // Remove old legend items
+//     while (ul.firstChild) {
+//       ul.firstChild.remove();
+//     }
 
-    // Reuse the built-in legendItems generator
-    const items = chart.options.plugins.legend.labels.generateLabels(chart);
+//     // Reuse the built-in legendItems generator
+//     const items = chart.options.plugins.legend.labels.generateLabels(chart);
 
-    items.forEach((item: any) => {
-      const li = document.createElement('li');
-      li.style.alignItems = 'center';
-      li.style.cursor = 'pointer';
-      li.style.display = 'flex';
-      li.style.flexDirection = 'row';
-      li.style.marginLeft = '10px';
+//     items.forEach((item: any) => {
+//       const li = document.createElement('li');
+//       li.style.alignItems = 'center';
+//       li.style.cursor = 'pointer';
+//       li.style.display = 'flex';
+//       li.style.flexDirection = 'row';
+//       li.style.marginLeft = '10px';
 
-      li.onclick = () => {
-        const { type } = chart.config;
-        if (type === 'pie' || type === 'doughnut') {
-          // Pie and doughnut charts only have a single dataset and visibility is per item
-          chart.toggleDataVisibility(item.index);
-        } else {
-          chart.setDatasetVisibility(item.datasetIndex, !chart.isDatasetVisible(item.datasetIndex));
-        }
-        chart.update();
-      };
+//       li.onclick = () => {
+//         const { type } = chart.config;
+//         if (type === 'pie' || type === 'doughnut') {
+//           // Pie and doughnut charts only have a single dataset and visibility is per item
+//           chart.toggleDataVisibility(item.index);
+//         } else {
+//           chart.setDatasetVisibility(item.datasetIndex, !chart.isDatasetVisible(item.datasetIndex));
+//         }
+//         chart.update();
+//       };
 
-      // Color box
-      const boxSpan = document.createElement('span');
-      boxSpan.style.background = item.fillStyle;
-      boxSpan.style.borderColor = item.strokeStyle;
-      boxSpan.style.borderWidth = item.lineWidth + 'px';
-      boxSpan.style.display = 'inline-block';
-      boxSpan.style.height = '20px';
-      boxSpan.style.marginRight = '10px';
-      boxSpan.style.width = '20px';
+//       // Color box
+//       const boxSpan = document.createElement('span');
+//       boxSpan.style.background = item.fillStyle;
+//       boxSpan.style.borderColor = item.strokeStyle;
+//       boxSpan.style.borderWidth = item.lineWidth + 'px';
+//       boxSpan.style.display = 'inline-block';
+//       boxSpan.style.height = '20px';
+//       boxSpan.style.marginRight = '10px';
+//       boxSpan.style.width = '20px';
 
-      // Text
-      const textContainer = document.createElement('p');
-      textContainer.style.color = item.fontColor;
-      textContainer.style.margin = '0';
-      textContainer.style.padding = '0';
-      textContainer.style.textDecoration = item.hidden ? 'line-through' : '';
+//       // Text
+//       const textContainer = document.createElement('p');
+//       textContainer.style.color = item.fontColor;
+//       textContainer.style.margin = '0';
+//       textContainer.style.padding = '0';
+//       textContainer.style.textDecoration = item.hidden ? 'line-through' : '';
 
-      const text = document.createTextNode(item.text);
-      textContainer.appendChild(text);
+//       const text = document.createTextNode(item.text);
+//       textContainer.appendChild(text);
 
-      li.appendChild(boxSpan);
-      li.appendChild(textContainer);
-      ul.appendChild(li);
-    });
-  },
-};
+//       li.appendChild(boxSpan);
+//       li.appendChild(textContainer);
+//       ul.appendChild(li);
+//     });
+//   },
+// };
 
 const LineChart: React.FC<ChartProps> = (props) => {
   const { loRaData, dataLabel } = props;
@@ -173,7 +173,11 @@ const LineChart: React.FC<ChartProps> = (props) => {
 
   return (
     <div>
-      <Line data={chartData} options={options} plugins={[htmlLegendPlugin]} />
+      <Line
+        data={chartData}
+        options={options}
+        //  plugins={[htmlLegendPlugin]}
+      />
       <div id="label-container"></div>
     </div>
   );
